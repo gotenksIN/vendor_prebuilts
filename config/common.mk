4753f050628f35f22f381fabeb7210ef9d56c0e6
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-10-20 15:18:31 +0530

    prebuilts: build with google apex only with gapps
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,7 @@
-# Inherit from apex config
+ifeq ($(BUILD_GAPPS),true)
+# Inherit from apex config only when built with Gapps
 $(call inherit-product, vendor/prebuilts/config/apex.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/prebuilts/overlay
+    vendor/prebuilts/overlay-gapps
+endif
