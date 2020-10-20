acf31f45f9cf2ae2f82e4d51d14fa65f7d7d08a2
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-10-20 15:21:38 +0530

    prebuilts: config: setup for gapps builds
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,10 @@ ifeq ($(BUILD_GAPPS),true)
 # Inherit from apex config only when built with Gapps
 $(call inherit-product, vendor/prebuilts/config/apex.mk)
 
+# Inherit our Gapps
+$(call inherit-product-if-exists, vendor/google/gms/config.mk)
+$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
+
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/prebuilts/overlay-gapps
 endif
